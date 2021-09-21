f7fb13d7f988c4707af66a5da9e105f4e6d7054a
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-22 00:27:47 +0200

    project pre setup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: false,
   arrowParens: 'avoid',
 };
