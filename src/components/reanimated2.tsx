f379e9e9091cc7221c244bd78944f4fd44f7854c
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-23 01:55:37 +0200

    Update reanimated2.tsx

--- src/components/reanimated2.tsx
+++ src/components/reanimated2.tsx
@@ -3,11 +3,9 @@ import {View, Text} from 'react-native-ui-lib';
 import Animated, {withSpring, useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
 
 import {Bounceable} from './bounceable';
-import {genNativeId} from '../services/navigation/sharedTransition';
-import {SharedTransitionId} from '../services/navigation/types';
 
 type Reanimated2Props = {
-  stID?: SharedTransitionId;
+  stID?: string;
 };
 
 export const Reanimated2: React.FC<Reanimated2Props> = ({stID}: Reanimated2Props) => {
@@ -26,7 +24,7 @@ export const Reanimated2: React.FC<Reanimated2Props> = ({stID}: Reanimated2Props
       <Animated.View style={[animatedStyles]}>
         <View center padding-s1>
           <Bounceable onPress={moveObject} activeScale={0.9}>
-            <View nativeID={genNativeId(stID)} center bg-primary padding-s8 br40>
+            <View center bg-primary padding-s8 br40>
               <Text whitish>Bounceable</Text>
             </View>
           </Bounceable>
