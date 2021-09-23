aacac549241ee280aaa0d4cf52c4cb5dbca2930e
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-23 18:25:10 +0200

    LogBox ignore logs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+LogBox.ignoreLogs(['EventEmitter.removeListener', '`new NativeEventEmitter()`']);
 
 AppRegistry.registerComponent(appName, () => App);
