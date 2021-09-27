41faa7827c7a81a4d8ce04574011401b147f25ed
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-09-27 03:51:33 +0200

    sharedTransitionScreens added

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,23 +1,44 @@
-import {ModalScreenLayouts, ScreenLayouts, TabScreenLayouts} from '../services/navigation/types';
+import {
+  ModalScreenLayouts,
+  ScreenLayouts,
+  SharedScreenLayouts,
+  SharedTransitionScreenLayouts,
+  TabScreenLayouts,
+} from '../services/navigation/types';
 
 import {Main} from './main';
 import {Settings} from './settings';
 import {Example} from './screen-sample';
-import {genRootNavigator, genStackNavigator, genTabNavigator} from '../services/navigation/help';
+import {
+  genRootNavigator,
+  genSharedStackNavigator,
+  genStackNavigator,
+  genTabNavigator,
+} from '../services/navigation/help';
+import {Shared} from './shared';
+import {SharedTo} from './sharedTo';
 
 // Describe your screens here
 export type Tabs = 'Main' | 'WIP' | 'Settings';
 export type Modal = 'ExampleModal';
+export type SharedTransitionScreen = 'Shared' | 'SharedTo';
+export type SharedScreen = 'ExampleShared';
 export type Screen = 'Main' | 'Example' | 'Settings';
 
 export type ModalProps = {
   ExampleModal: undefined;
 };
+export type SharedScreenProps = {
+  ExampleShared: undefined;
+};
 export type ScreenProps = {
   Main: undefined;
   Example: ExampleScreenProps;
   Settings: undefined;
-} & ModalProps;
+  Shared: undefined;
+  SharedTo: undefined;
+} & ModalProps &
+  SharedScreenProps;
 
 // Screens
 const screens: ScreenLayouts = {
@@ -43,8 +64,45 @@ const screens: ScreenLayouts = {
     }),
   },
 };
-const HomeStack = () => genStackNavigator([screens.Main, screens.Example]);
-const ExampleStack = () => genStackNavigator([screens.Example]);
+const sharedTransitionScreens: SharedTransitionScreenLayouts = {
+  Shared: {
+    name: 'Shared',
+    component: Shared,
+    options: () => ({
+      title: 'Shared Transition',
+    }),
+  },
+  SharedTo: {
+    name: 'SharedTo',
+    component: SharedTo,
+    options: () => ({
+      title: 'SharedTo Transition',
+    }),
+    sharedElements: (route, otherRoute, showing) => {
+      // const {item} = route.params;
+      // return [`item.${item.id}.photo`];
+      console.log('showing', showing);
+      return ['image', 'reanimated2'];
+    },
+  },
+};
+const ExampleSharedStack = () =>
+  genSharedStackNavigator([sharedTransitionScreens.Shared, sharedTransitionScreens.SharedTo]);
+const sharedScreens: SharedScreenLayouts = {
+  ExampleShared: {
+    name: 'ExampleShared',
+    component: ExampleSharedStack,
+    options: () => ({
+      title: 'Shared Transition',
+      headerLargeTitle: false,
+      headerTransparent: false,
+      headerBlurEffect: undefined,
+    }),
+  },
+};
+const HomeStack = () =>
+  genStackNavigator([screens.Main, screens.Example, sharedScreens.ExampleShared]);
+const ExampleStack = () => genStackNavigator([screens.Example, sharedScreens.ExampleShared]);
 const SettingsStack = () => genStackNavigator([screens.Settings]);
 const ExampleModalStack = () => genStackNavigator([screens.Main, screens.Example]);
 
